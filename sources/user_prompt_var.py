b85ead5537a087ed8b0d5979ea215cf09839b96f
Aamin Gem <gem.aamin@gmail.com>
2023-09-14 03:29:51 +0200

    Draft 4

--- sources/user_prompt_var.py
+++ sources/user_prompt_var.py
@@ -1,11 +1,11 @@
-match_variable = "S"
-possible_affixes = "^"
 definitions = [{'index': 0, 
                 'identifier': 'S', 
                 'description': 'Soundness operator'},
                 {'index': 1, 
                 'identifier': 'S^', 
                 'description': 'Dual operator of S'}]
+match_variable = "S"
+possible_affixes = "^"
 context = "(*@$\rightarrow$@*), (*@$\leftrightarrow$@*) and truth values ((*@$\top$@*), (*@$\bot$@*)) are introduced as
 abbreviations. We denote by E (Dual operator of E [^])^, <|S|>^, and A^
 the dual operators corresponding to E,"
